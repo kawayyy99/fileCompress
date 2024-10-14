33d87545a743d46fca39cdab074f47c6549d6651
Wayan Putu Suarjana <babassiahaan@gmail.com>
2024-10-14 14:39:04 +0700

    update navtab link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/header';
-import FileCompressor from './components/fileCompress';  // Import komponen kompresi file
 import NavTabs from './components/navTab';
-import FileConverter from './components/fileConverter';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <NavTabs />
-      <div className="row">
-        <div className="col-md-12 bg-light">
-          <FileCompressor />
-        </div>
-        <div className="col-md-12 bg-light">
-          <FileConverter />
-        </div>
-      </div>
+      
     </div>
   );
 }
--- src/components/navTab.js
+++ src/components/navTab.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FileCompressor from './fileCompress'; 
+import FileConverter from './fileConverter';
 
 const NavPills = () => {
+  // State to manage the active tab
+  const [activeTab, setActiveTab] = useState('compression');
+
+  // Function to handle tab switching
+  const handleTabChange = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
-    <div className="container-fluid p-0">
-      <ul className="nav nav-pills nav-fill m-3">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">File Compression</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Extension Converter</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link Converter Downloader</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="container-fluid p-0">
+        <ul className="nav nav-pills nav-fill m-3">
+          <li className="nav-item">
+            {/* Change active tab to 'compression' when this link is clicked */}
+            <a 
+              className={`nav-link ${activeTab === 'compression' ? 'active' : ''}`}
+              onClick={() => handleTabChange('compression')} 
+              href="#"
+            >
+              File Compression
+            </a>
+          </li>
+          <li className="nav-item">
+            {/* Change active tab to 'converter' when this link is clicked */}
+            <a 
+              className={`nav-link ${activeTab === 'converter' ? 'active' : ''}`}
+              onClick={() => handleTabChange('converter')} 
+              href="#"
+            >
+              Extension Converter
+            </a>
+          </li>
+          <li className="nav-item">
+            {/* Change active tab to 'downloader' when this link is clicked */}
+            <a 
+              className={`nav-link ${activeTab === 'downloader' ? 'active' : ''}`} 
+              onClick={() => handleTabChange('downloader')} 
+              href="#"
+            >
+              Link Converter Downloader
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">
+              Disabled
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Render content based on active tab */}
+      <div className="container-fluid p-0">
+        <div className="row">
+          <div className="col-md-12 bg-light">
+            {activeTab === 'compression' && <FileCompressor />}
+            {activeTab === 'converter' && <FileConverter />}
+            {activeTab === 'downloader' && <p>Link Converter Downloader content will go here</p>}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
